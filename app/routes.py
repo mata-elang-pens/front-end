b1f68734440c75dd5e432722b44b52cde7c513eb
dimasmamot <dimas.rizky.hp@gmail.com>
2018-10-19 06:49:32 +0700

    push

--- app/routes.py
+++ app/routes.py
@@ -51,7 +51,10 @@ def index():
 
     for i in range(len(data['sensors'])):
         #Check per sensors
-        data['sensors'][i]['status'] = "running" 
+        url_check = "http://{}/api/sensor/v1.0/checkstatus/{}".format(os.environ.get('API_HOST'), data['sensors'][i]['device_id'])
+        r_check = requests.post(url_check, auth=(session['token'], "pass"))
+        data_check = json.loads(r_check)
+        data['sensors'][i]['status'] = data_check['status']
 
     return render_template(
         'index.html', 
